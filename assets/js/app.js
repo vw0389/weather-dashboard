a1cba379a481bc5bc83f1160094f3642e2814f52
Victor Weinert <dev@vweinert.com>
2021-10-04 16:33:46 -0500

    added a lot of features

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,143 @@
-function getDay(city) {
+var key = "de9e65903924e7323a2e2738fc9a391b"
+var history = {};
+// TODO
+// Fix local storage issues, it always gets a dictionary of length 1 from local storage for some reason
+// style the webpage
+$(".search").click(async function () {
+    var city = $(".textSearch").val().trim();
+    var today = new Date();
+    if (city) {
+        $(".textSearch").val("");
+        var weatherUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid=" + key;
+        var firstCall = await get(weatherUrl);
+        if (firstCall === 1) {
+            alert("City not found");
+            return;
+
+        } else if (firstCall === 2) {
+            alert("an error occured reaching the API");
+            return;
+        }
+        var lat = firstCall.coord.lat;
+        var lon = firstCall.coord.lon;
+        var oneCallUrl = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&units=imperial&appid=" + key + "&exclude=hourly,minutely";
+        var secondCall = await get(oneCallUrl);
+        
+        if (secondCall === 1) {
+            alert("City not found");
+            return;
+
+        } else if (secondCall === 2) {
+            alert("an error occured reaching the API");
+            return;
+        }
+        var days = [];
+        for (var i = 0; i < 6; i++) {
+            days[i] = {
+                "date": today.setDate(today.getDate() + 1),
+                "type": secondCall.daily[i].weather[0].main,
+                "temp": secondCall.daily[i].temp.day,
+                "wind": secondCall.daily[i].wind_speed,
+                "humidity": secondCall.daily[i].humidity,
+                "uvi": secondCall.daily[i].uvi,
+            }
+        }
+        saveHistory(firstCall.name,days);
+        loadHistory();
+        display(firstCall.name);
+    } else {
+        alert("please enter a name of a city to get the weather of");
+        return;
+    }
+
+});
+async function get(url) {
+    try {
+        let response = await fetch(url);
+        let json = await response.json();
+        if (json.cod === '404') {
+            return 1;
+        }
+        return json;
+
+    } catch (error) {
+        console.log("an error occured trying to get json");
+        return 2
+    }
+}
+// save history to localstorage
+function saveHistory(cityName, days){
+    history[cityName] = days;
+    localStorage.setItem('weather_history', JSON.stringify(history));
     
 }
+// load history from localstorage
+function loadHistory () {
+    var json_string = localStorage.getItem('weather_history');
+    
+    if (json_string) {
+        history = JSON.parse(json_string);
+    }
+}
+
+function display(city) {
+    var days = history[city];
+    var today = new Date(days[0].date);
+
+    $(".city-stats").empty();
+    $(".5-day-forecast").empty();
+    if (days[0].type === 'Rain') {
+        var span = $("<i></i>").attr("class", "fas fa-cloud-rain fa-2x");
+    } else if (days[0].type === 'Clear') {
+        var span = $("<i></i>").attr("class", "fas fa-sun");
+    } else if (days[0].type === 'Clouds') {
+        var span = $("<i></i>").attr("class", "fas fa-cloud");
+    } else if (days[0].type === 'Snow') {
+        var span = $("<i></i>").attr("class", "fas fa-snowflake");
+    } else {
+        var span = $("<i></i>").attr("class", "fas fa-sun");
+        console.log("unkown condition, display()");
+    }
+    if (parseFloat(days[0].uvi) < 3.0) {
+        var color = "green";
+    } else if (parseFloat(days[0].uvi) < 5.0){
+        var color = "yellow";
+    } else {
+        var color = "red"
+    }
+    var cityNameDate = $("<h3></h3>").text(city + " (" + today.toISOString().substring(0,10) + ")");
+    span.appendTo(cityNameDate);
+    var temp = $("<p></p>").text("Temp: " + days[0].temp + " °F");
+    var wind = $("<p></p>").text("Wind: " + days[0].wind + " MPH");
+    var humidity = $("<p></p>").text("Humidity: " + days[0].humidity + " %");
+    var uv = $("<p></p>").text("UV Index: " + days[0].uvi);
+    uv.attr("style", "background-color:" + color);
+    $(".city-stats").append(cityNameDate,temp,wind,humidity,uv);
+    var fiveDayForecast = $("<h3></h3>").text("5-day forecast:")
+    $(".5-day-forecast").append(fiveDayForecast);
+    for ( var i = 1; i < 6; i++) {
+        if (days[i].type === 'Rain') {
+            var span = $("<i></i>").attr("class", "fas fa-cloud-rain fa-2x");
+        } else if (days[i].type === 'Clear') {
+            var span = $("<i></i>").attr("class", "fas fa-sun");
+        } else if (days[i].type === 'Clouds') {
+            var span = $("<i></i>").attr("class", "fas fa-cloud");
+        } else if (days[i].type === 'Snow') {
+            var span = $("<i></i>").attr("class", "fas fa-snowflake");
+        } else {
+            var span = $("<i></i>").attr("class", "fas fa-sun");
+            console.log("unkown condition, display()");
+        }
+        var date = new Date(days[i].date);
+        var div = $("<div></div>");
+        var dateEl = $("<h4></h4>").text("(" + date.toISOString().substring(0,10) + ")");
+        var type = span;
+        var temp = $("<p></p>").text("Temp: " + days[i].temp + " °F");
+        var wind = $("<p></p>").text("Wind: " + days[i].wind + " MPH");
+        var humidity = $("<p></p>").text("Humidity: " + days[i].humidity + " %");
+        div.append(dateEl,type,temp,wind,humidity);
+        $(".5-day-forecast").append(div);
+    }
+}
+
+loadHistory();
